18963cdf6263da2fe549b38f0ef388dbd14b3804
Taha <farjia@192.168.6.90>
2026-07-28 10:59:49 +0100

    refactor: remove embedded permissions overview

--- src/init-embedded-tasks.js
+++ src/init-embedded-tasks.js
@@ -9,7 +9,6 @@ import storeFactory from './store/main.js'
 import ClickOutside from 'vue-click-outside'
 import { translate, translatePlural } from '@nextcloud/l10n'
 import Board from './components/board/Board.vue'
-import SharingTabSidebar from './components/board/SharingTabSidebar.vue'
 import ReportingDashboard from './components/board/ReportingDashboard.vue'
 import { BoardApi } from './services/BoardApi.js'
 import './shared-init.js'
@@ -52,54 +51,11 @@ const EmbeddedBoardWrapper = {
 	}
 }
 
-const EmbeddedPermissionsWrapper = {
-	name: 'EmbeddedPermissionsWrapper',
-	props: ['boardId'],
-	data() {
-		return {
-			loading: true,
-			error: null,
-		}
-	},
-	computed: {
-		board() {
-			return this.$store.state.currentBoard
-		}
-	},
-	async created() {
-		try {
-			await this.$store.dispatch('loadBoardById', this.boardId)
-		} catch (e) {
-			this.error = 'Failed to load permissions'
-		} finally {
-			this.loading = false
-		}
-	},
-	render(h) {
-		if (this.loading) {
-			return h('div', { class: 'emptycontent' }, [
-				h('div', { class: 'icon icon-loading' }),
-				h('h2', 'Loading permissions...')
-			])
-		}
-		if (this.error) {
-			return h('div', { class: 'emptycontent' }, [
-				h('h2', this.error)
-			])
-		}
-		return h(SharingTabSidebar, {
-			props: {
-				board: this.board,
-			}
-		})
-	}
-}
-
 window.OCA.Deck.EmbeddedTasks = {
 	mount({ el, boardId }) {
 		const pinia = createPinia()
 		const store = storeFactory()
-		
+
 		// Set full app to false so that the sidebar/navigation acts properly in embedded contexts
 		store.commit('setFullApp', false)
 
@@ -135,44 +91,6 @@ window.OCA.Deck.EmbeddedTasks = {
 				if (vm.$refs.wrapper) {
 					vm.$refs.wrapper.boardId = Number(newBoardId)
 				}
-			}
-		}
-	}
-}
-
-window.OCA.Deck.EmbeddedPermissionsOverview = {
-	mount({ el, boardId }) {
-		const pinia = createPinia()
-		const store = storeFactory()
-
-		store.commit('setFullApp', false)
-
-		// Create wrapper container to avoid replacing el
-		const mountEl = document.createElement('div')
-		mountEl.style.height = '100%'
-		mountEl.style.width = '100%'
-		el.appendChild(mountEl)
-
-		const vm = new Vue({
-			el: mountEl,
-			store,
-			pinia,
-			provide() {
-				return {
-					boardApi,
-				}
-			},
-			render: h => h(EmbeddedPermissionsWrapper, {
-				props: {
-					boardId: Number(boardId),
-				}
-			})
-		})
-
-		return {
-			destroy() {
-				vm.$destroy()
-				el.innerHTML = ''
 			}
 		}
 	}
